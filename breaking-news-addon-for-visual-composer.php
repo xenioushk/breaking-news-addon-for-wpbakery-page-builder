6ba192377b14220393e42ab0ffdf96acbff726c7
Md Mahbub Alam Khan <hkhan.cse@gmail.com>
2023-01-13 22:27:53 +0600

    - remove the documentation folder. (#1)
    Co-authored-by: Md Mahbub Alam Khan <md.m.khan@tuni.fi>

--- breaking-news-addon-for-visual-composer.php
+++ breaking-news-addon-for-visual-composer.php
@@ -1,10 +1,10 @@
 <?php
 
-/**
- * Plugin Name: Breaking News Addon For Visual Composer
+
+ * Plugin Name: Breaking News Addon For WP Bakery Page Builder
  * Plugin URI:  http://codecanyon.net/user/xenioushk
- * Version: 1.0.1
- * Description: Easy way to create & display breaking news any where of your site from Visual Composer.
+ * Version: 1.0.2
+ * Description: Easy way to create & display breaking news any where of your site from WP Bakery Page Builder.
  * Author: xenioushk
  * Author URI:  http://codecanyon.net/user/xenioushk
  * Text Domain: bnm_vc
@@ -15,9 +15,9 @@ if (!defined('ABSPATH')) {
   die('-1');
 }
 include_once(ABSPATH . 'wp-admin/includes/plugin.php');
-define("BNM_VC_PLUGIN_TITLE", 'Breaking News Addon For Visual Composer');
+define("BNM_VC_PLUGIN_TITLE", 'Breaking News Addon For WP Bakery Page Builder');
 define("BNM_VC_PLUGIN_DIR", plugins_url() . '/breaking-news-addon-for-visual-composer/');
-define("BNM_VC_PLUGIN_VERSION", '1.0.1');
+define("BNM_VC_PLUGIN_VERSION", '1.0.2');
 define("BNM_VC_PLUGIN_PRODUCTION_STATUS", 1); // Change this value in to 0 in Devloper mode :)
 define('BNM_PLUGIN_INSTALLED_VERSION', get_option('bnm_plugin_version'));
 
@@ -28,7 +28,7 @@ function notice_bnm_dependencies()
   if (!function_exists('vc_map')) {
     echo '
         <div class="updated">
-          <p>' . sprintf(__('<strong>%s</strong> requires <strong><a href="http://codecanyon.net/item/visual-composer-page-builder-for-wordpress/242431?ref=xenioushk" target="_blank">Visual Composer</a></strong> plugin to be installed and activated on your site.', 'vc_extend'), BNM_VC_PLUGIN_TITLE) . '</p>
+          <p>' . sprintf(__('<strong>%s</strong> requires <strong><a href="https://1.envato.market/VKEo3" target="_blank">WP Bakery Page Builder</a></strong> plugin to be installed and activated on your site.', 'vc_extend'), BNM_VC_PLUGIN_TITLE) . '</p>
         </div>';
   }
 
@@ -36,7 +36,7 @@ function notice_bnm_dependencies()
 
     echo '
         <div class="updated">
-          <p>' . sprintf(__('<strong>%s</strong> requires <strong><a href="http://codecanyon.net/item/bwl-post-to-breaking-news-manager/6335199?ref=xenioushk" target="_blank">BWL Breaking News Manager</a></strong> plugin to be installed and activated on your site. Minimum version <b>1.1.0</b> required ! ', 'vc_extend'), BNM_VC_PLUGIN_TITLE) . '</p>
+          <p>' . sprintf(__('<strong>%s</strong> requires <strong><a href="https://1.envato.market/bnm-wp" target="_blank">BWL Breaking News Manager</a></strong> plugin to be installed and activated on your site. Minimum version <b>1.1.0</b> required ! ', 'vc_extend'), BNM_VC_PLUGIN_TITLE) . '</p>
         </div>';
   }
 }
