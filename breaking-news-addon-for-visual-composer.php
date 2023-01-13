3bdfe3681d87ff7d7d97e976ae8ed525dde17048
Md Mahbub Alam Khan <md.m.khan@tuni.fi>
2023-01-13 18:20:15 +0200

    - remove the documentation folder.

--- breaking-news-addon-for-visual-composer.php
+++ breaking-news-addon-for-visual-composer.php
@@ -1,48 +1,48 @@
 <?php
-/**
-* Plugin Name: Breaking News Addon For Visual Composer
-* Plugin URI:  http://codecanyon.net/user/xenioushk
-* Version: 1.0.1
-* Description: Easy way to create & display breaking news any where of your site from Visual Composer.
-* Author: xenioushk
-* Author URI:  http://codecanyon.net/user/xenioushk
-* Text Domain: bnm_vc
-* Domain Path: /languages/
-*/
 
-if ( ! defined( 'ABSPATH' ) ) {
-    die( '-1' );
+/**
+ * Plugin Name: Breaking News Addon For WP Bakery Page Builder
+ * Plugin URI:  http://codecanyon.net/user/xenioushk
+ * Version: 1.0.2
+ * Description: Easy way to create & display breaking news any where of your site from WP Bakery Page Builder.
+ * Author: xenioushk
+ * Author URI:  http://codecanyon.net/user/xenioushk
+ * Text Domain: bnm_vc
+ * Domain Path: /languages/
+ */
+
+if (!defined('ABSPATH')) {
+  die('-1');
 }
-include_once( ABSPATH . 'wp-admin/includes/plugin.php' );
-define("BNM_VC_PLUGIN_TITLE", 'Breaking News Addon For Visual Composer');
+include_once(ABSPATH . 'wp-admin/includes/plugin.php');
+define("BNM_VC_PLUGIN_TITLE", 'Breaking News Addon For WP Bakery Page Builder');
 define("BNM_VC_PLUGIN_DIR", plugins_url() . '/breaking-news-addon-for-visual-composer/');
-define("BNM_VC_PLUGIN_VERSION", '1.0.1');
+define("BNM_VC_PLUGIN_VERSION", '1.0.2');
 define("BNM_VC_PLUGIN_PRODUCTION_STATUS", 1); // Change this value in to 0 in Devloper mode :)
 define('BNM_PLUGIN_INSTALLED_VERSION', get_option('bnm_plugin_version'));
 
 
+function notice_bnm_dependencies()
+{
 
-function notice_bnm_dependencies() {
- 
-    if ( ! function_exists('vc_map')) {
-        echo '
+  if (!function_exists('vc_map')) {
+    echo '
         <div class="updated">
-          <p>' . sprintf(__('<strong>%s</strong> requires <strong><a href="http://codecanyon.net/item/visual-composer-page-builder-for-wordpress/242431?ref=xenioushk" target="_blank">Visual Composer</a></strong> plugin to be installed and activated on your site.', 'vc_extend'), BNM_VC_PLUGIN_TITLE) . '</p>
+          <p>' . sprintf(__('<strong>%s</strong> requires <strong><a href="https://1.envato.market/VKEo3" target="_blank">WP Bakery Page Builder</a></strong> plugin to be installed and activated on your site.', 'vc_extend'), BNM_VC_PLUGIN_TITLE) . '</p>
         </div>';
-    }
- 
-     if ( ! class_exists('BWL_Breaking_News_Manager') ) {
+  }
+
+  if (!class_exists('BWL_Breaking_News_Manager')) {
 
     echo '
         <div class="updated">
-          <p>' . sprintf(__('<strong>%s</strong> requires <strong><a href="http://codecanyon.net/item/bwl-post-to-breaking-news-manager/6335199?ref=xenioushk" target="_blank">BWL Breaking News Manager</a></strong> plugin to be installed and activated on your site. Minimum version <b>1.1.0</b> required ! ', 'vc_extend'), BNM_VC_PLUGIN_TITLE) . '</p>
+          <p>' . sprintf(__('<strong>%s</strong> requires <strong><a href="https://1.envato.market/bnm-wp" target="_blank">BWL Breaking News Manager</a></strong> plugin to be installed and activated on your site. Minimum version <b>1.1.0</b> required ! ', 'vc_extend'), BNM_VC_PLUGIN_TITLE) . '</p>
         </div>';
-
-     }
+  }
 }
 
 
-add_action('admin_notices', 'notice_bnm_dependencies' );
- 
+add_action('admin_notices', 'notice_bnm_dependencies');
+
 include_once dirname(__FILE__) . '/includes/bnm-vc-element.php';
-load_plugin_textdomain('bnm_vc', false, plugin_basename(dirname(__FILE__)) . '/languages/'); 
+load_plugin_textdomain('bnm_vc', false, plugin_basename(dirname(__FILE__)) . '/languages/');
